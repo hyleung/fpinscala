25b2ecec041f0e357cf12292d693377c30b9cf07
Ho Yan Leung <hy.leung@gmail.com>
2015-06-08 11:43:53 -0700

    Implements List.tail

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,7 +50,11 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case (Cons(h,t)) => t
+    case _ => List()
+  }
+
 
   def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
 
---
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -0,0 +1,22 @@
+package fpinscala.datastructures
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by hyleung on 15-06-08.
+ */
+class ListSpec extends FlatSpec with Matchers{
+  behavior of "List.tail"
+  it should "return tail of list" in {
+    val list = List(1,2,3,4)
+    List.tail(list) should be (List(2,3,4))
+  }
+  it should "return empty list for tail of singleton list" in {
+    val list = List(1)
+    List.tail(list) should be (List())
+  }
+  it should "return empty list for tail of empty list" in {
+    val list = List()
+    List.tail(list) should be (List())
+  }
+}
