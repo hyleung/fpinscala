7057c7a7d33db752791b6dfe1b8fe1aa5a61f80f
Ho Yan Leung <hy.leung@gmail.com>
2015-10-04 07:53:52 -0700

    Adds monoid law tests for other monoids

--- exercises/src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ exercises/src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -10,16 +10,30 @@ import org.scalatest.{FlatSpec, Matchers}
  * To change this template use File | Settings | File Templates.
  */
 class MonoidSpec extends FlatSpec with Matchers{
-	behavior of "int addition monoid"
-	it should "obey monoid laws" in {
+	behavior of "Monoid laws"
+	it should "hold for int addition monoid" in {
 		val m = Monoid.intAddition
 		val g = Gen.choose(Int.MinValue,Int.MaxValue)
 		Monoid.monoidLaws(m)(g)
 	}
-	behavior of "int multiplication monoid"
-	it should "obey monoid laws" in {
+	it should "hold for int multiplication monoid" in {
 		val m = Monoid.intMultiplication
 		val g = Gen.choose(Int.MinValue,Int.MaxValue)
+		Monoid.monoidLaws(m)(g)
+	}
+	it should "hold for boolean AND monoid" in {
+		val m = Monoid.booleanAnd
+		val g = Gen.boolean
+		Monoid.monoidLaws(m)(g)
+	}
+	it should "hold for boolean OR monoid" in {
+		val m = Monoid.booleanOr
+		val g = Gen.boolean
+		Monoid.monoidLaws(m)(g)
+	}
+	it should "hold for string addition monoid" in {
+		val m = Monoid.stringMonoid
+		val g = Gen.choose(Int.MinValue,Int.MaxValue).map(_.toString)
 		Monoid.monoidLaws(m)(g)
 	}
 
