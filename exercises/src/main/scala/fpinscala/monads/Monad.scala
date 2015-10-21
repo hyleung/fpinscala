5ee1b15a94a919b73a0856c8162b71bf38b621e9
Ho Yan Leung <hy.leung@gmail.com>
2015-10-21 06:46:27 -0700

    Sequence function for Monad[_]

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -34,7 +34,8 @@ trait Monad[M[_]] extends Functor[M] {
   def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
-  def sequence[A](lma: List[M[A]]): M[List[A]] = ???
+  def sequence[A](lma: List[M[A]]): M[List[A]] =
+    lma.foldLeft(unit(List[A]()))((acc,a) => map2(a,acc)((y,z) => y :: z))
 
   def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = ???
 
