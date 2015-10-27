3cb56f96911dc06026e613cfaeef72fc13a51231
Ho Yan Leung <hy.leung@gmail.com>
2015-10-27 07:02:44 -0700

    Adds reader monad

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -145,8 +145,9 @@ case class Id[A](value: A) {
 
 object Reader {
   def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
-    def unit[A](a: => A): Reader[R,A] = ???
-    override def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] = ???
+    def unit[A](a: => A): Reader[R,A] = Reader(_ => a)
+    override def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
+        Reader(r => f(st.run(r)).run(r))
   }
 }
 
