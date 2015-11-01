5d0787f3fbe33c2f62865715ee94f366ed83f6be
Ho Yan Leung <hy.leung@gmail.com>
2015-11-01 06:27:20 -0800

    Implements validation applicative

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -107,7 +107,17 @@ object Applicative {
       a zip b map f.tupled
   }
 
-  def validationApplicative[E]: Applicative[({type f[x] = Validation[E,x]})#f] = ???
+  def validationApplicative[E]: Applicative[({type f[x] = Validation[E,x]})#f] = new Applicative[({type f[x] = Validation[E, x]})#f] {
+    def unit[A](a: => A): Validation[E, A] = Success(a)
+
+    override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = (fa,fb) match {
+      case (Success(a),Success(b)) => Success(f(a,b))
+      case (Failure(h1,t1),Failure(h2,t2)) => Failure(h1, t1 ++ Vector(h2) ++ t2)
+      case (_, e@Failure(_,_)) => e
+      case (e@Failure(_,_),_) => e
+    }
+
+  }
 
   type Const[A, B] = A
 
